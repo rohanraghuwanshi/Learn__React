0b440a8cde8cac5d7f6f8b368ff6c54560542860
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-17 22:30:11 +0530

    feat : add monster filter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -29,9 +30,21 @@ class App extends Component {
   }
 
   render() {
+
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter(
+      monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type='search'
+          placeholder='Search Monsters'
+          onChange={e => this.setState({ 'searchField': e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
