f8677997d2efc5b3b51052a681594654eb252593
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-17 23:39:27 +0530

    feat : final touch

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='Search Monsters'
           handleChange={e => this.setState({ 'searchField': e.target.value })}
