fa6f6eea4e6cc4df8d199064c75ec5417bb482f5
rohanraghuwanshi <rohanraghuvanshi001@gmail.com>
2021-05-17 22:43:23 +0530

    feat : add seperate search component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -39,10 +40,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='Search Monsters'
-          onChange={e => this.setState({ 'searchField': e.target.value })}
+          handleChange={e => this.setState({ 'searchField': e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
